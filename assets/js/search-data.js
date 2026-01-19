c9fb1fae4034dee230e87102fd3ad619b596fcce
dhyeyshah28 <56397262+dhyeyshah28@users.noreply.github.com>
2026-01-19 03:14:42 +0000

    Deploying to gh-pages from @ dhyeyshah28/dhyeyshah28.github.io@83472e45c98b5c2c4ad85dff02133764ba747265 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,23 +28,16 @@ ninja.data = [{
           description: "Resume - Dhyey Shah.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/assets/pdf/resume.pdf";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "nav-hobbies",
+          title: "hobbies",
+          description: "My Hobbies - Dhyey Shah.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/hobbies/";
+          },
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
